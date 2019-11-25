53e0fa0a4cc136bba1971b95870b48d6eb21fa39
ela <ela.malinowska@idego.pl>
2019-11-25 08:36:52 +0100

    Virtual environment created. PostgreSQL database configured

---
+++ movies_app/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviesapp',
+        'USER': 'postgres',
+        'PASSWORD': 'ZXCbnm246&%#',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
