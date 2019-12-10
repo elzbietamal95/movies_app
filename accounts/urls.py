2baa5cccde5ff5f7f99692b7070c5b1b22d5d647
ela <ela.malinowska@idego.pl>
2019-12-10 19:20:12 +0100

    Login class-base view created (only for valid credentials). CSS styles for users-list view added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from accounts import views
-from accounts.views import UserLogoutView
+from accounts.views import UserLogoutView, UserLoginView
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('', views.user_panel, name='panel'),
-    path('login/', views.user_login, name='login'),
+    path('login/', UserLoginView.as_view(), name='login'),
     path('logout/', UserLogoutView.as_view(), name='logout'),
     path('registration/', views.user_signup, name='signup'),
     path('user-list/', views.user_list_view, name='user-list'),
