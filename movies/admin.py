6ef176f0dcfbeb221cb92c8a30b529f7cb7c6659
ela <ela.malinowska@idego.pl>
2020-02-03 10:44:07 +0100

    RoleInLine for MovieAdmin added

--- movies/admin.py
+++ movies/admin.py
@@ -2,11 +2,16 @@ from django.contrib import admin
 from .models import Actor, Role, Movie
 
 
+class RoleInLine(admin.TabularInline):
+    model = Role
+    extra = 3
+
+
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['title', 'slug', 'year_of_production', 'image', 'added_by']
     list_filter = ['year_of_production']
+    inlines = [RoleInLine]
 
 
 admin.site.register(Actor)
-admin.site.register(Role)
 admin.site.register(Movie, MovieAdmin)
